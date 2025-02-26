0a175fe221c24f1a70f061958d1bede8432e3cd8
Miyashooooo <cstrike.asia@gmail.com>
2025-02-26 16:15:34 +0800

    fix: Popup Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,9 @@ const Header: React.FC<HeaderProps> = React.memo(({ title, onClose }) => {
 
   return (
     <div className={styles['header']}>
-      {title && <div className={styles['title']}>{title}</div>}
+      <div className={styles['titleBox']}>
+        {title && <span className={styles['title']}>{title}</span>}
+      </div>
       <div className={styles['buttons']}>
         <div className={styles['minimize']} />
         <div
